90e9334575d37288569d1c78b88fd9eb78090c92
Emmanuel Yapura <eyapura96@gmail.com>
2025-07-14 19:02:05 -0300

    agrega endpoint para scrapear y cargar la base de datos

--- routes/app.py
+++ routes/app.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
 from scraper.musimundo_scraper import MusimundoScrapper
+from sqlalchemy.orm import Session
+from database.schemas.schema import ProductoSchema
+from database.database import get_base
+from database.crud.crud import get_or_create_categoria, get_or_create_subcategoria, create_productos
 
 router = APIRouter()
 scraper = MusimundoScrapper()
@@ -26,3 +30,30 @@ def get_products_subcategorias(subcategoria):
     except Exception as e:
         return {"message" : f"No se encontraron productos en la subcategoria {subcategoria}, Error {e}"}
     
+@router.post('/scraper/{categoria_nombre}')
+def scrapear_guardar_productos(categoria_nombre: str, subcategoria_nombre: str = None, db: Session = Depends(get_base)):
+    try:
+        if subcategoria_nombre:
+            productos_data = scraper.obtener_productos_subcategoria(subcategoria_nombre)
+        else:
+            productos_data = scraper.obtener_productos_por_categoria(categoria_nombre)
+
+        productos_schema = [ProductoSchema(**data) for data in productos_data]
+
+        categoria = get_or_create_categoria(db, categoria_nombre)
+
+        subcategoria_id = None
+        
+        if subcategoria_nombre:
+            subcategoria = get_or_create_subcategoria(db, subcategoria_nombre, categoria.id)
+            subcategoria_id = subcategoria.id
+
+        productos = create_productos(db, productos_schema, subcategoria_id)
+
+        return {
+            "message": f"{len(productos)} productos guardados exitosamente",
+            "categoria": categoria_nombre,
+            "subcategoria": subcategoria_nombre if subcategoria_nombre else None
+        }
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))

a28d2ee862ab3f6bad14cb90e531173723218408
Emmanuel Yapura <eyapura96@gmail.com>
2025-07-17 10:49:53 -0300

    modifica funcion para no obtener duplicados y actualizar ids subcategorias

--- database/crud/crud.py
+++ database/crud/crud.py
@@ -20,20 +20,37 @@ def get_or_create_subcategoria(db: Session, nombre: str, categoria_id: int):
         db.refresh(subcategoria)
     return subcategoria
 
-def create_productos(db: Session, productos_data: list[ProductoSchema], subcategoria_id: int = None):
-    productos = []
-    for data in productos_data:
-        producto = Producto(
-            name = data.name,
-            description = data.description,
-            cashPrice = data.cashPrice,
-            price = data.price,
-            onlyDelivery = data.onlyDelivery,
-            brandInfo = data.brandInfo,
-            url = data.url,
-            subcategoria_id = subcategoria_id
-        )
-        productos.append(producto)
-    db.add_all(productos)
+def create_productos(db: Session, productos_data: list[ProductoSchema], categoria_id: int, subcategoria_id: int = None):
+    for producto in productos_data:
+        producto_existente = db.query(Producto).filter(Producto.url == producto.url).first()
+
+        if producto_existente:
+            producto_existente.name = producto.name
+            producto_existente.price = producto.price
+            producto_existente.cashPrice = producto.cashPrice
+            producto_existente.onlyDelivery = producto.onlyDelivery
+            producto_existente.brandInfo = producto.brandInfo
+            producto_existente.description = producto.description
+            producto_existente.categoria_id = categoria_id
+
+            # Solo actualiza subcategoria si estaba vacía
+            if not producto_existente.subcategoria_id and subcategoria_id:
+                producto_existente.subcategoria_id = subcategoria_id
+        
+        else:
+            nuevo = Producto(
+                name = producto.name,
+                description = producto.description,
+                cashPrice = producto.cashPrice,
+                price = producto.price,
+                onlyDelivery = producto.onlyDelivery,
+                brandInfo = producto.brandInfo,
+                url = producto.url,
+                subcategoria_id = subcategoria_id,
+                categoria_id = categoria_id
+            )
+            db.add(nuevo)
     db.commit()
-    return productos
+
+    return productos_data
+    
